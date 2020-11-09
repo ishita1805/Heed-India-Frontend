63a8f806b20e4f043b60cedd3b69aa1abd98e7e3
Ishita Kabra <ishitakabra1805@gmail.com>
2020-11-09 22:28:29 +0530

    home page

--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import BottomNav from '../../components/bottomNav/BottomNav'
+import './home.css'
 
 const Home = (props) => {
     useEffect(() => {
@@ -9,10 +10,15 @@ const Home = (props) => {
     <div className="in-body-2 col">
         <div className="grow">
             {/* content comes here remove H1 tag */}
-            <h1>Home</h1>
-            {props.state}
+            <h1><span className="green-home">HEED</span> INDIA</h1>
+            <p>Do we have a moral obligation to help people we do not know? If so, why?</p>
+            <h2><span className="green-home">#be</span><span className="green-home-1">the</span><span className="green-home-2">change</span></h2>
+            <button className="button-green">Contact Us</button>
         </div>
-        <BottomNav/>
+        <div className="small-screen-bot-nav">
+            <BottomNav/>
+        </div>
+        
     </div>
     )
 }
