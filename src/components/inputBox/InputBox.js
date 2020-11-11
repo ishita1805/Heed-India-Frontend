e516a2f6ee2f5a2bee4e92fdf053fb582e92abb2
KKapoor0801 <58838197+KKapoor0801@users.noreply.github.com>
2020-11-12 02:03:21 +0530

    buttons

--- src/components/inputBox/InputBox.js
+++ src/components/inputBox/InputBox.js
@@ -5,7 +5,7 @@ function InputBox(props) {
         <Fragment>
         <label>
             <input onChange={props.onChange}
-            className={props.className}>{props.label}</input>
+            className={props.className} value={props.value}>{props.label}</input>
         </label>
         </Fragment>
     )
