f158de855676a68cd7a5d3326bf84f56adad2d06
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-12-07 15:03:20 +0530

    workpage

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './profileMedia.css'
-import {Link} from 'react-router-dom'
 
 
 var img = "https://static.wixstatic.com/media/1f2275_1ec26c62602b4ef09365c9dc35636047~mv2.jpg/v1/fill/w_204,h_222,al_c,q_80,usm_0.66_1.00_0.01/1f2275_1ec26c62602b4ef09365c9dc35636047~mv2.webp"
@@ -19,10 +18,10 @@ export default function Profile() {
           </div>
     
           <div className="profile-user-settings">
-    
+          <form  action="https://www.instagram.com/heedindia.ngo/">
             <h1 className="profile-user-name">heedindia.org</h1>
-    
-           <a href="https://www.instagram.com/heedindia.ngo/"><button className="btn profile-edit-btn" >Follow</button></a>
+              <input  className="profile-edit-btn" type="submit" value="Follow" />
+            </form>
           </div>
     
           <div className="profile-stats">
